c07d350aa1b714a2e90d7a6551654bc41b96a55f
Ann Rose Cherian <annrose@Anns-MacBook-Pro.local>
2020-10-02 01:35:19 +0530

    added myself reduced time complexity while finding min amd max by comparing in pairs

--- Array_max_min.c
+++ Array_max_min.c
@@ -1,32 +1,58 @@
 #include <stdio.h>
-
-int maximum(int A[], int n){
-    int mx = A[0];
-    for (int i = 1; i < n; i++)
+struct min_max
+{
+    int mn;
+    int mx;
+};
+struct min_max findmnmx(int A[], int n)
+{
+    struct min_max mnmx;
+    int i;
+    if(n%2 == 0)
     {
-        if(mx < A[i]){
-            mx = A[i];
+        if (A[0] > A[1])
+        {
+            mnmx.mx = A[0];
+            mnmx.mn = A[1];
         }
+        else
+        {
+            mnmx.mn = A[0];
+            mnmx.mx = A[1];
+        }
+        i = 2;
     }
-    return mx;
-    
-}
-int minimum(int A[], int n){
-    int mn = A[0];
-    for (int i = 1; i < n; i++)
+    else
     {
-        if(mn > A[i]){
-            mn = A[i];
-        }
+        mnmx.mn = mnmx.mx = A[0];
+        i = 1;
     }
-    return mn;
-    
+    while(i < n-1)
+    {
+        if(A[i] > A[i+1])
+        {
+            if(A[i]>mnmx.mx)
+                mnmx.mx = A[i];
+            if(A[i+1]<mnmx.mn)
+                mnmx.mn = A[i+1];
+        }
+        else
+        {
+            if(A[i+1] > mnmx.mx)
+                mnmx.mx = A[i+1];
+            if(A[i] < mnmx.mn)
+                mnmx.mn = A[i];
+        }
+        i+=2;
+    }
+    return mnmx;
 }
 
 int main(){
     int A[] = {1, 3, 4, 1}; 
     int size = (sizeof(A)/sizeof(int));
-    //printing the sum
-    printf("%d",minimum(A, size) + maximum(A, size) );
+    struct min_max mnmx = findmnmx(A, size);
+    //printing the sum of max and min elements
+    printf("%d",mnmx.mn + mnmx.mx );
     return 0;
 }
