b11f7722ee4619fc684d50674a1654934666c9f6
Saksham Nauriyal <68427921+saksham715@users.noreply.github.com>
2020-10-01 15:46:36 +0530

    Create multiThreading.py

---
+++ multiThreading.py
@@ -0,0 +1,26 @@
+from time import sleep
+from threading import Thread
+
+class Hello(Thread):
+    def run():
+    for i in ramge(5):
+        print("Hello")
+        sleep(1)
+        
+class Hi(Thread)
+    def run():
+    for i in range(5):
+    print("Hi")
+    sleep(1)
+    
+hello = Hello()
+hi = Hi()
+
+hello.start()
+sleep(0.2)
+hi.start()
+
+hello.join()
+hi.join()
+
+print("Bye")
