4637c98b9abd391b2e8515933c099ed5d5755580
Agrim2411 <56195843+Agrim2411@users.noreply.github.com>
2020-10-02 16:17:06 +0530

    Update merge_sort.cpp
    Python version using recursion

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,70 +1,43 @@
-#include<bits/stdc++.h>
-using namespace std;
-int merge(int arr[],int start, int mid,int mid2, int end)
-{
-	int size1 = mid2 - start; 
-    int size2 = end - mid; 
-  
-    int arr1[size1], arr2[size2]; 
-  
-    for (int i = 0; i < size1; i++) 
-        arr1[i] = arr[start + i]; 
-    for (int j = 0; j < size2; j++) 
-        arr2[j] = arr[mid2 + j]; 
-  	int i = 0; 
-    int j = 0; 
-    int k = start;
-    while (i < size1 && j < size2) { 
-        if (arr1[i] <= arr2[j]) { 
-            arr[k] = arr1[i]; 
-            i++; 
-        } 
-        else { 
-            arr[k] = arr2[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-  
-    while (i < size1) { 
-        arr[k] = arr1[i]; 
-        i++; 
-        k++; 
-	} 
-  
-    while (j < size2) { 
-        arr[k] = arr2[j]; 
-        j++; 
-        k++; 
-    } 
-}
-int mergesort(int arr[], int start, int end)
-{
-	if(start == end)
-		return arr[start];
-	
-	else
-	{
-		int mid = (start + end )/2;
-		mergesort(arr,start,mid);
-		mergesort(arr,mid+1,end);
-		merge(arr,start,mid, mid+1 , end);
-	}
-}
-int main()
-{
-	int arr[5] = { 1, 4, 5, 3, 2};
-	cout<<"The array before sorting is: ";
-	for(int i=0;i<5;i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	cout<<"\n";
-	cout<<"The array after sorting is: ";
-	mergesort(arr,0,4);
-	for(int i=0;i<5;i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	return 0;
-}
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+
+myList = [54,26,93,17,77,31,44,55,20]
+mergeSort(myList)
+print(myList)
