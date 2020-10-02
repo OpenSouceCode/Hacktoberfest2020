7c3d0e681529f3690b4d955927911ecd939ac6ad
Aparna-07 <54182737+Aparna-07@users.noreply.github.com>
2020-10-02 13:58:32 +0530

    Update Array_max_min.c
    Added comments to the program to enhance readability.

--- Array_max_min.c
+++ Array_max_min.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 int maximum(int A[], int n){
-    int mx = A[0];
+    int mx = A[0]; //declared mx as first element in the array making it as maximum element in the array
     for (int i = 1; i < n; i++)
     {
-        if(mx < A[i]){
-            mx = A[i];
+        if(mx < A[i]){ //checking if mx is lesser than present element in the array
+            mx = A[i]; //updating the value of mx to maximum element in the array
         }
     }
-    return mx;
+    return mx; //returning the max element in the array
     
 }
 int minimum(int A[], int n){
-    int mn = A[0];
+    int mn = A[0]; //declared mn as first element in the array making it as minimum element in the array
     for (int i = 1; i < n; i++)
     {
-        if(mn > A[i]){
-            mn = A[i];
+        if(mn > A[i]){ //checking if mn is greater than present element in the array
+            mn = A[i]; //updating the value of mn to minimum element in array
         }
     }
-    return mn;
+    return mn; //returning the min element in the array
     
 }
 
 int main(){
-    int A[] = {1, 3, 4, 1}; 
-    int size = (sizeof(A)/sizeof(int));
+    int A[] = {1, 3, 4, 1};  //declared and initialized the array A
+    int size = (sizeof(A)/sizeof(int)); 
     //printing the sum
-    printf("%d",minimum(A, size) + maximum(A, size) );
+    printf("%d",minimum(A, size) + maximum(A, size) ); //printing the sum of greatest and smallest element in the array
     return 0;
 }
