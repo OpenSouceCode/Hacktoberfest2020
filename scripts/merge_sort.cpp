e1b36994fbd2ac0a6724038d91476cf067c567b9
shruti-17 <52755579+shruti-17@users.noreply.github.com>
2020-10-06 00:46:32 +0530

    Update merge_sort.cpp

--- scripts/merge_sort.cpp
+++ scripts/merge_sort.cpp
@@ -1,70 +1,84 @@
 #include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int merge(int arr[],int start, int mid,int mid2, int end)
+ 
+int a[100];
+void merge(int,int,int);
+void merge_sort(int low,int high)
 {
-	int size1 = mid2 - start; 
-    int size2 = end - mid; 
-  
-    int arr1[size1], arr2[size2]; 
-  
-    for (int i = 0; i < size1; i++) 
-        arr1[i] = arr[start + i]; 
-    for (int j = 0; j < size2; j++) 
-        arr2[j] = arr[mid2 + j]; 
-  	int i = 0; 
-    int j = 0; 
-    int k = start;
-    while (i < size1 && j < size2) { 
-        if (arr1[i] <= arr2[j]) { 
-            arr[k] = arr1[i]; 
-            i++; 
-        } 
-        else { 
-            arr[k] = arr2[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-  
-    while (i < size1) { 
-        arr[k] = arr1[i]; 
-        i++; 
-        k++; 
-	} 
-  
-    while (j < size2) { 
-        arr[k] = arr2[j]; 
-        j++; 
-        k++; 
-    } 
+ int mid;
+ if(low<high)
+ {
+  mid=(low+high)/2;
+  merge_sort(low,mid);
+  merge_sort(mid+1,high);
+  merge(low,mid,high);
+ }
 }
-int mergesort(int arr[], int start, int end)
+void merge(int low,int mid,int high)
 {
-	if(start == end)
-		return arr[start];
-	
-	else
-	{
-		int mid = (start + end )/2;
-		mergesort(arr,start,mid);
-		mergesort(arr,mid+1,end);
-		merge(arr,start,mid, mid+1 , end);
-	}
+ int h,i,j,b[100],k;
+ h=low;
+ i=low;
+ j=mid+1;
+ 
+ while((h<=mid)&&(j<=high))
+ {
+  if(a[h]<=a[j])
+  {
+   b[i]=a[h];
+   h++;
+  }
+  else
+  {
+   b[i]=a[j];
+   j++;
+  }
+  i++;
+ }
+ if(h>mid)
+ {
+  for(k=j;k<=high;k++)
+  {
+   b[i]=a[k];
+   i++;
+  }
+ }
+ else
+ {
+  for(k=h;k<=mid;k++)
+  {
+   b[i]=a[k];
+   i++;
+  }
+ }
+ for(k=low;k<=high;k++) a[k]=b[k];
 }
 int main()
 {
-	int arr[5] = { 1, 4, 5, 3, 2};
-	cout<<"The array before sorting is: ";
-	for(int i=0;i<5;i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	cout<<"\n";
-	cout<<"The array after sorting is: ";
-	mergesort(arr,0,4);
-	for(int i=0;i<5;i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	return 0;
+ int num,i;
+ 
+ cout<<"Enter no.of elements you want to sort:";
+ cin>>num;
+ 
+ cout<<"Enter "<< num <<" Elements : "<<endl;
+ 
+ for(i=1;i<=num;i++)
+ {
+  cout<<i<<" Element : ";
+  cin>>a[i] ;
+ }
+ merge_sort(1,num);
+ cout<<endl;
+ cout<<"After Merge Sort, Sorted Array is :"<<endl;
+ 
+ for(i=1;i<=num;i++)
+ {
+      cout<<a[i]<<" ";
+ }
+ 
+ cout<<endl;
+ 
+return 0;
 }
+
